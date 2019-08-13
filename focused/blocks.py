c192dbc221480e71785623624e52515c863a4c94
SuperHelios <20555025+Imperator26@users.noreply.github.com>
2019-08-13 09:52:43 +0200

    Use the article tag to select the content

--- focused/blocks.py
+++ focused/blocks.py
@@ -11,22 +11,35 @@ def title(html):
     return title_reg.findall(html)[0]
 
 
+def article(html):
+    article_reg = re.compile(r"<article.*?>[\w\W]*?</article>", re.MULTILINE)
+    return article_reg.search(html).group(0)
+
+
 def _remove_block(text, p):
     return p.sub("", text)
 
 
 def clean(html, no_scripts):
-    comments = re.compile(r"<!--([\w\W]*?)-->", re.MULTILINE)
-    scripts = re.compile(r"<script.*?>([\w\W]*?)<\/script>", re.MULTILINE)
+    # Compile patterns
+    comments_reg = re.compile(r"<!--([\w\W]*?)-->", re.MULTILINE)
+    scripts_reg = re.compile(r"<script.*?>([\w\W]*?)<\/script>", re.MULTILINE)
+    no_scripts_reg = re.compile(r"(<noscript>|<\/noscript>)", re.MULTILINE)
     content = re.compile(
         r"(<((?P<p>p)|(?P<h>h[1-6])).*?>([\w\W]*?)<\/((?P=p)|(?P=h))>)",
         re.MULTILINE
     )
 
-    html = _remove_block(html, comments)
+    html = _remove_block(html, comments_reg)
     if no_scripts:
-        html = _remove_block(html, scripts)
+        html = _remove_block(html, scripts_reg)
+        html = _remove_block(html, no_scripts_reg)
 
-    matches = content.findall(html)
+    # Find article
+    body = article(html)
 
-    return "".join([x[0] for x in matches])
+    if body is None:
+        matches = content.findall(html)
+        return "".join([x[0] for x in matches])
+    else:
+        return body
